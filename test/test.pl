0eddc6b26834bcb60da1c571bebdb9ca745234a2
Jonathan Cross <jcross@gmail.com>
2016-11-30 02:46:00 +0100

    Major improvments to unit tests!

--- test/test.pl
+++ test/test.pl
@@ -9,35 +9,109 @@ my $script_name = "../../j-renamer.pl"; # Path from test_data/ subdir.
 my $test_data = "test_data/";
 my $test_results = "../results";
 
-runTests();
+_runTests();
+_tearDown();
 
 exit;
 
 ################################################################################
 # Runs tests
-sub runTests {
+sub _runTests {
+  printf "Running unit tests:\n";
   _testDefault();
+  _testUpper();
+  _testLower();
+  _testPatternFoo();
+  _testInAndStart();
+  _testForcedPaddingViaPattern();
+  _testForcedPaddingViaStart();
 }
 
 sub _testDefault {
-  my $name = 'default';
-  my $expected = 'a.txt 1.txt
+  _runTest('',
+           'a.txt 1.txt
 b.jpg 2.jpg
 b.txt 2.txt
 c.txt 3.txt
 d.TXT 4.TXT
-';
-  _runTest($name, '', $expected);
+');
+}
+
+sub _testUpper {
+  _runTest('--ext:upper',
+           'a.txt 1.TXT
+b.jpg 2.JPG
+b.txt 2.TXT
+c.txt 3.TXT
+d.TXT 4.TXT
+');
+}
+
+sub _testLower {
+  _runTest('--ext:lower',
+           'a.txt 1.txt
+b.jpg 2.jpg
+b.txt 2.txt
+c.txt 3.txt
+d.TXT 4.txt
+');
+}
+
+sub _testPatternFoo {
+  _runTest('--out:Foo_#',
+           'a.txt Foo_1.txt
+b.jpg Foo_2.jpg
+b.txt Foo_2.txt
+c.txt Foo_3.txt
+d.TXT Foo_4.TXT
+');
+}
+
+sub _testInAndStart {
+  # Tests if complex --in pattern works and simple --start example.
+  _runTest('--in:"*.{txt,TXT}" --start:6',
+            'a.txt 6.txt
+b.txt 7.txt
+c.txt 8.txt
+d.TXT 9.TXT
+');
+}
+
+sub _testForcedPaddingViaPattern {
+  _runTest('--out:###',
+           'a.txt 001.txt
+b.jpg 002.jpg
+b.txt 002.txt
+c.txt 003.txt
+d.TXT 004.TXT
+');
+}
+
+sub _testForcedPaddingViaStart {
+  # Tests if more complex start correctly triggers forced padding.
+  _runTest('--start:7',
+           'a.txt 07.txt
+b.jpg 08.jpg
+b.txt 08.txt
+c.txt 09.txt
+d.TXT 10.TXT
+');
 }
 
 ################################################################################
-# Testing template
+# Runs a given unit test and checks the result.
+# @param $params   Parameters to send to j-renamer
+# @param $expected Expected result of rename.
 sub _runTest {
-  my ($name, $params, $expected) = @_;
+  my ($params, $expected) = @_;
+  my ($name) = ((caller(1))[3] =~ /main::_test([A-z]+)$/);
   my $results_file = "results_$name.txt";
   # Cleans up output from j-renamer.pl
-  my $sedRe = '/=>/!d; s/  \([abcd].[A-z]\{3\}\).*\([0-9].[A-z]\{3\}\).*/\1 \2/g';
+  my $sedRe = '/=>/!d; s/  \([abcd].[A-z]\{3\}\).*=>[^A-z0-9 _-]*\([A-z0-9 _-]*\.[A-z]\{3\}\).*/\1 \2/g';
   printf " • Testing $name:";
+  #                                               Use tee here for debug ---+
+  #                                                                         |
+  #                                                                         V
   my @args = ("cd $test_data; $script_name * $params <<< n | sed '${sedRe}' > ../$results_file");
   system(@args) == 0
       or die "system @args failed: $?";
@@ -45,7 +119,11 @@ sub _runTest {
   if ($result eq $expected) {
     printf " PASS\n";
   } else {
-    printf " FAIL\n";
+    printf " FAILED!
+    Try running this:
+    cd $test_data; $script_name * $params <<< n | sed '${sedRe}'; echo 'Here were actual results:'; cat ../$results_file
+    ";
+    exit 1;
   }
 }
 
@@ -59,5 +137,12 @@ sub _loadResults {
   }
   close($fh);
   return $results;
+}
+
+sub _tearDown {
+  # Delete all the test result files.
+  my @args = ("rm results_*.txt");
+  system(@args) == 0
+      or die "system @args failed: $?";
 }
 __END__
